5c5196e16bac8caab456ba7537d77ff982a51ba8
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-08 12:52:52 +0700

    +

--- main.js
+++ main.js
@@ -11,6 +11,11 @@ const game = new Game([
         hero: ZhangFei, 
         name: "เตียวหุย", 
         controller: AIController//HumanController//AIController
+    },
+    {
+        hero: LiuBei, 
+        name: "เล่าปี่2", 
+        controller: AIController
     }
 ]);
 // เรียกใช้เมธอด start() เพื่อเริ่มรัน Game Loop / ระบบการเล่นของเกม
