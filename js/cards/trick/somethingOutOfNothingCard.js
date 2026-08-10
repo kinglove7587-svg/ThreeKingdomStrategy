8de4246f740e97336536878e3af031164cf46d98
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-10 10:37:09 +0700

    +

--- js/cards/trick/somethingOutOfNothingCard.js
+++ js/cards/trick/somethingOutOfNothingCard.js
@@ -1,14 +1,14 @@
 class SomethingOutOfNothingCard extends TrickCard{
     constructor(suit, number){
+        // กำหนดชื่อการ์ดอุบาย "บังเกิดมีสิ่ง" พร้อมดอก (suit) และตัวเลข (number)
         super("บังเกิดมีสิ่ง", suit, number);
     }
-
+    //
     use(player, game){
         game.log(player.name + " จั่วการ์ด 2 ใบ");
-
+        // จั่วการ์ดจากกองกลางเข้ามือ 2 ใบ
         player.drawCard(game.deck);
         player.drawCard(game.deck);
-
         return true;
     }
 }
