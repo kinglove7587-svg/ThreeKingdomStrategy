02694cb3fdb4f7091d497a69887a38fcd975352b
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-12 06:29:57 +0700

    +

--- js/cards/trick/knowEnemyCard.js
+++ js/cards/trick/knowEnemyCard.js
@@ -2,4 +2,12 @@ class KnowEnemyCard extends TrickCard{
     constructor(suit, number){
         super("รู้เขารู้เรา", suit, number);
     }
+    // ตรวจสอบว่าเป้าหมายสามารถถูกเลือกได้หรือไม่
+    canTarget(player, target){
+        // ห้ามเลือกตัวเองเป็นเป้าหมาย
+        if(player === target){
+            return false;
+        }
+        return true;
+    }
 }
