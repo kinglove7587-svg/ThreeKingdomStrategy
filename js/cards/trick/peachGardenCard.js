4a366771a7f80b04d0a4639f0073456b051ea836
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-12 06:01:55 +0700

    +

--- js/cards/trick/peachGardenCard.js
+++ js/cards/trick/peachGardenCard.js
@@ -2,4 +2,12 @@ class PeachGardenCard extends TrickCard{
     constructor(suit, number){
         super("คำสาบานสวนท้อ", suit, number);
     }
+    // สั่งใช้งานการ์ดคำสาบานสวนท้อ โดยฟื้นฟูพลังชีวิต (HP)
+    use(player, game){
+        // วนลูปผู้เล่นทุกคนในเกม (รวมถึงตัวเอง) เพื่อฟื้นฟู HP คนละ 1 หน่วย
+        for(const target of game.players){
+            target.recoverHp(1);
+        }
+        return true;
+    }
 }
