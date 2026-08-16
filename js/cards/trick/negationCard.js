4554826661c1bdb630e879c8cbebbe676879b292
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-16 21:21:41 +0700

    +canReact(context)

--- js/cards/trick/negationCard.js
+++ js/cards/trick/negationCard.js
@@ -3,4 +3,20 @@ class NegationCard extends TrickCard{
     constructor(suit, number){
         super("หักล้าง", suit, number);
     }
+    // ตรวจสอบว่าการ์ดหักล้างใบนี้สามารถใช้ตอบโต้การ์ดใน Context ได้หรือไม่
+    canReact(context){
+
+        if(!context){
+            return false;
+        }
+
+        if(!context.card){
+            return false;
+        }
+        // อนุญาตให้ตอบโต้ถ้าเป็นการ์ดประเภท Trick หรือการ์ดหักล้าง
+        return (
+            context.card instanceof TrickCard || 
+            context.card.name === "หักล้าง"
+        );
+    }
 }
