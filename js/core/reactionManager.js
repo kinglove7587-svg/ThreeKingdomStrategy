fc605e8970715532731b066dc92d50eb3447d38b
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-16 21:48:04 +0700

    +

--- js/core/reactionManager.js
+++ js/core/reactionManager.js
@@ -111,6 +111,7 @@ class ReactionManager{
         }
         // นำการ์ดออกจากมือของผู้ตอบ
         responder.hand.cards.splice(cardIndex, 1);
+        this.context.canceled = true;
         console.log(responder.name + " ใช้ " + card.name);
         return card;
     }
