74fadae2784037401b157f70c5ea54414a665bf7
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-19 21:37:35 +0700

    +

--- js/cards/equipment/frostSwordCard.js
+++ js/cards/equipment/frostSwordCard.js
@@ -5,4 +5,15 @@ class FrostSwordCard extends WeaponCard{
 
         this.addSkill(new FrostSwordSkill());
     }
+    // ทำงานเมื่อผู้เล่นสวมใส่อาวุธกระบี่น้ำแข็ง
+    onEquip(player){
+
+        const skill = this.skills[0];
+        // ตรวจสอบว่ามีสกิลนี้อยู่แล้วหรือยัง เพื่อป้องกันการเพิ่มสกิลซ้ำ
+        if(!player.skills.includes(skill)){
+            player.addSkill(skill);
+        }
+        console.log(player.name + " สวมกระบี่น้ำแข็ง");
+        
+    }
 }
