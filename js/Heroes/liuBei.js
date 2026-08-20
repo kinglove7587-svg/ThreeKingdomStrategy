6544832c3eecf137bd49d17050e1ca8e02aa9c97
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-20 12:14:34 +0700

    +

--- js/Heroes/liuBei.js
+++ js/Heroes/liuBei.js
@@ -11,8 +11,8 @@ class LiuBei extends Player{ // เล่าปี่
         //this.setChained(true);
         //this.equipWeapon(new SerpentSpearCard("♣️", 7));
         //this.equipArmor(new SilverLionHelmetCard("♣️", 7));
-        this.hand.addCard(new WoodenCartCard("♠️", 1));
-        this.hand.addCard(new WoodenCartCard("♠️", 1));
-        this.hand.addCard(new SlashCard("♠️", 1));
+        //this.hand.addCard(new WoodenCartCard("♠️", 1));
+        //this.hand.addCard(new WoodenCartCard("♠️", 1));
+        //this.hand.addCard(new SlashCard("♠️", 1));
     }
 }
