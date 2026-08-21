5756c505b322253f434fd9e61272b3f005d83126
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-21 23:49:07 +0700

    +

--- js/cards/trick/restAndReorganizationCard.js
+++ js/cards/trick/restAndReorganizationCard.js
@@ -3,9 +3,14 @@ class RestAndReorganizationCard extends TrickCard{
     constructor(suit, number){
         super("พักพลจัดทัพ", suit, number);
     }
-    //
+    // ประมวลผลการใช้การ์ดพักพลจัดทัพ
     use(player, game){
-        //
+        // ให้ผู้ใช้จั่วการ์ด 2 ใบ
+        player.drawCard(game.deck);
+        player.drawCard(game.deck);
+        game.log(player.name + " จั่วการ์ด 2 ใบ");
+        game.ui.render();
+        return true;
     }
     getDescription(){
         return "จั่วการ์ด 2 ใบ แล้วเลือกการ์ดจากมือ 2 ใบเพื่อทิ้ง";
