33bf0bb3643cc91e965a26cc9c0bcc75f4e94307
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-22 17:37:47 +0700

    +

--- js/skills/base/passiveSkill.js
+++ js/skills/base/passiveSkill.js
@@ -3,4 +3,8 @@ class PassiveSkill extends Skill{
     constructor(name){
         super(name);
     }
+    // 
+    getAttackDistanceModifier(player, attacker, target, game){
+        return 0;
+    }
 }
