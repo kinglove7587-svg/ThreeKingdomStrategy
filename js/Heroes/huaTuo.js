c4add04f3a2be16b48d35cb655e1ae621d3e365a
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-24 04:52:12 +0700

    +

--- js/Heroes/huaTuo.js
+++ js/Heroes/huaTuo.js
@@ -1,13 +1,13 @@
 class HuaTuo extends Player{
 
     constructor(game, controllerClass){
-        super("ฮิวโต๋", game, controllerClass);
+        super("ฮัวโต๋", game, controllerClass);
 
         this.maxHp = 3;
         this.hp = 3;
         this.faction = "Qun";
         this.gender = "male";
-        this.abiliryDescription = 
+        this.abilityDescription = 
             "First Aid (หัตถ์โอสถช่วยชีวิต)\n" +
             "ในช่วง Play Phase สามารถใช้การ์ดสีแดง 1 ใบเสมือน [ยา] ได้\n\n" +
             "Prodigal Healer (หมอเทพเมตตา)\n" +
