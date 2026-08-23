326a6fad0e277c16b46d10b17e80cc6334042ae1
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-24 04:50:39 +0700

    +

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const game = new Game([
         controller: HumanController//AIController//
     },
     {
-        hero: SunQuan, 
+        hero: HuaTuo, 
         controller: HumanController//AIController//
     },
     {
