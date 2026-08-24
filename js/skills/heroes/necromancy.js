50a27039f3257e271d9578b289057a97ccab0faf
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-24 15:29:10 +0700

    +

--- js/skills/heroes/necromancy.js
+++ js/skills/heroes/necromancy.js
@@ -3,4 +3,22 @@ class Necromancy extends TriggerSkill{
     constructor(){
         super("Necromancy");
     }
+    // ลงทะเบียนที่จะทำงานเมื่อไพ่ถูกเปิดเผย
+    register(eventManager, player){
+
+        const callback = (context) => {
+            if(!context){
+                return;
+            }
+            console.log("Necromancy Judge Event =", context);
+            console.log("Necromancy Owner =", player.name);
+            console.log("Judge Player =", context.player?.name);
+            console.log("Judge Card =", context.card);
+        };
+        this.registerListener(
+            eventManager, 
+            "judgeCardRevealed", 
+            callback
+        );
+    }
 }
