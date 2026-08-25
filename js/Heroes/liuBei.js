d915003766c3e521df74faacf6ffb6dc58dfa911
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-25 19:26:49 +0700

    +

--- js/Heroes/liuBei.js
+++ js/Heroes/liuBei.js
@@ -16,8 +16,8 @@ class LiuBei extends Player{ // เล่าปี่
             "สามารถขอให้ตัวละครฝ่าย Shu ใช้หรือเล่น [โจมตี] แทนตนได้ หากตัวละครนั้นเต็มใจ";
         this.addSkill(new Rende());
         //this.setChained(true);
-        //this.equipWeapon(new YinYangSwordsCard("♣️", 7));
-        //this.equipArmor(new EightTrigramsArmor("♣️", 7));
+        this.equipWeapon(new YinYangSwordsCard("♣️", 7));
+        this.equipArmor(new EightTrigramsArmor("♣️", 7));
         //this.hand.addCard(new WoodenCartCard("♠️", 1));
         //this.hand.addCard(new LightningCard("♠️", 1));
         //this.hand.addCard(new SlashCard("♠️", 1));
