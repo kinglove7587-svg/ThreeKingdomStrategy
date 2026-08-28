51596825c50a5c41f7dda20b0b741c2e42bd9f5e
kinglove7587-svg <kinglove7587@gmail.com>
2026-08-29 04:29:59 +0700

    +

--- js/core/game.js
+++ js/core/game.js
@@ -553,6 +553,11 @@ class Game {
     }
     // จัดการผลลัพธ์หลังผู้เล่นมนุษย์ทำ Action (ลงการ์ด)
     afterHumanAction(success){
+        //------------------------
+        console.log("[AHA] CALLED", {
+            success: success,
+            actionLockedBefore: this.actionLocked
+        });
         // Action จบแล้ว ปลดล็อก End Turn
         this.finishAction();
         // ถ้าเกมจบแล้ว ให้หยุดการทำงานทันที
@@ -1082,6 +1087,14 @@ class Game {
         this.pendingAction = null;
         
         const result = action.resume();
+        //-----------------------------
+        console.log("[resumeAction] FINISHED", {
+            result: result,
+            autoAfterHumanAction: action.autoAfterHumanAction,
+            pendingAction: this.pendingAction,
+            triggerWaiting: this.triggerResolutionQueue.isWaiting(),
+            actionLocked: this.actionLocked
+        });
         if(
             action.autoAfterHumanAction && 
             this.pendingAction === null && 
@@ -1095,6 +1108,12 @@ class Game {
     }
     // ดำเนิน Trigger และ Action ที่หยุดไว้ต่อ
     resumeTriggerAndAction(damage){
+        //-------------------
+        console.log("[RTA] ENTER", {
+            pendingAction: !!this.pendingAction,
+            actionLocked: this.actionLocked,
+            triggerWaiting: this.triggerResolutionQueue.isWaiting()
+        });
 
         const nextTrigger = this.resumeTriggerResolution(damage);
         if(nextTrigger){
@@ -1104,9 +1123,28 @@ class Game {
         const pendingPlayer = this.pendingAction?.player;
         const autoAfterHumanAction = this.pendingAction?.autoAfterHumanAction;
         const hasPendingAction = !!this.pendingAction;
+        // debug
+        console.log("[resumeTriggerAndAction] BEFORE resume", {
+            hasPendingAction: hasPendingAction,
+            pendingPlayer: pendingPlayer?.name,
+            autoAfterHumanAction: autoAfterHumanAction,
+            actionLocked: this.actionLocked
+        });
+        //------------------------
+        console.log("[RTA] BEFORE resumeAction", {
+            pendingPlayer: this.pendingAction?.player?.name,
+            autoAfterHumanAction: this.pendingAction?.autoAfterHumanAction
+        });
         const actionResult = hasPendingAction
             ? this.resumeAction()
             : true;
+        //debug
+        console.log("[resumeTriggerAndAction] AFTER resume", {
+            actionResult: actionResult,
+            pendingAction: this.pendingAction,
+            triggerWaiting: this.triggerResolutionQueue.isWaiting(),
+            actionLocked: this.actionLocked
+        });
 
         if(this.triggerResolutionQueue.isWaiting()){
             return true;
@@ -1117,6 +1155,19 @@ class Game {
             pendingPlayer &&
             pendingPlayer.controller instanceof HumanController
         ){
+            //--------------
+            console.log("[resumeTriggerAndAction] CALL afterHumanAction", {
+                pendingPlayer: pendingPlayer?.name,
+                isHuman: pendingPlayer?.controller?.isHuman?.(),
+                instanceofHumanController:
+                    pendingPlayer?.controller instanceof HumanController,
+                actionResult: actionResult
+            });
+            console.log("[RTA] CALL afterHumanAction", {
+                hasPendingAction: hasPendingAction,
+                pendingPlayer: pendingPlayer?.name,
+                actionResult: actionResult
+            });
             this.afterHumanAction(actionResult);
             return actionResult;
         }
