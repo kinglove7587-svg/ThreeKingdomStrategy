47bcec33bba08c128ea037ae79613c3193fab727
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-02 16:49:57 +0700

    +

--- js/skills/equipment/greenDragonBladeSkill.js
+++ js/skills/equipment/greenDragonBladeSkill.js
@@ -14,12 +14,6 @@ class GreenDragonBladeSkill extends TriggerSkill{
     }
     // ตรวจสอบว่า Slash ของผู้สวมอาวุธนี้ถูกหลบหรือไม่
     onBeforeSlashHit(player, context, resolution){
-         console.log(
-            "[DEBUG GreenDragon] beforeSlashHit",
-            context.canceled,
-            context.source?.name,
-            context.target?.name
-        ); 
 
         if(!context.canceled){
             return;
@@ -34,9 +28,6 @@ class GreenDragonBladeSkill extends TriggerSkill{
         );
         // หยุด Slash เดิมไว้ก่อน เพื่อรอการตัดสินใจใช้ Trigger Choice
         context.waitingTrigger = true;
-        console.log(
-            "[DEBUG GreenDragon] Trigger ผ่าน → เปิดหน้าต่างเลือก"
-        );
         // เรียก Controller เริ่มถามผู้เล่นว่าจะใช้ความสามารถง้าวมังกรเขียวหรือไม่
         player.controller.startTriggerChoice(this, 
             {
