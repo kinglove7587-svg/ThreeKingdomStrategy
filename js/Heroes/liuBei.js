76afb52ba9cf6c7f41537ba360609e136e2893fa
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-05 17:44:39 +0700

    +

--- js/Heroes/liuBei.js
+++ js/Heroes/liuBei.js
@@ -17,7 +17,7 @@ class LiuBei extends Player{ // เล่าปี่
             
         this.addSkill(new Rende());
         //this.setChained(true);
-        //this.equipWeapon(new YinYangSwordsCard("♣️", 7));
+        //this.equipWeapon(new FrostSwordCard("♣️", 7));
         //this.equipArmor(new EightTrigramsArmor("♣️", 7));
         //this.hand.addCard(new BarbarianCard("♠️", 1));
         //this.hand.addCard(new RationsDepletedCard("♠️", 1));
