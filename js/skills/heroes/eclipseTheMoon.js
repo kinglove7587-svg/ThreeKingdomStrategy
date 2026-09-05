caa0a7bb2873c341047429830c74bc4c6a123f54
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-05 18:36:00 +0700

    +

--- js/skills/heroes/eclipseTheMoon.js
+++ js/skills/heroes/eclipseTheMoon.js
@@ -3,20 +3,14 @@ class EclipseTheMoon extends TriggerSkill{
     constructor(){
         super("Eclipse The Moon");
     }
-    register(eventManager, player){
+    onTurnEnd(player, game){
 
-        const callback = (turnEndPlayer) => {
-            // ต้องเป็นเจ้าของสกิลที่กำลังจบเทิร์น
-            if(turnEndPlayer !== player){
-                return;
-            }
-            player.game.log(player.name + " จั่วการ์ดจาก Eclipse the Moon 1 ใบ");
-            player.drawCard(player.game.deck);
-        };
-        this.registerListener(
-            eventManager, 
-            "onTurnEnd", 
-            callback
+        if(player !== this.owner){
+            return;
+        }
+        game.log(
+            player.name + " จั่วการ์ดจาก Eclipse the Moon 1 ใบ"
         );
+        player.drawCard(game.deck);
     }
 }
