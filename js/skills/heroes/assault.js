ffcb807d6c3786f901bc408bcc0e1cb4f487cc48
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-06 09:41:54 +0700

    +

--- js/skills/heroes/assault.js
+++ js/skills/heroes/assault.js
@@ -9,6 +9,24 @@ class Assault extends TriggerSkill{
         if(player !== this.owner){
             return;
         }
-        game.pauseDrawPhase(player);
+        if(player.controller instanceof HumanController){
+            game.pauseDrawPhase(player);
+            player.controller.startTriggerChoice(
+                this, 
+                {
+                    drawPhase: true
+                }
+            );
+        }
+    }
+    //
+    resolveChoice(player, game, context, useSkill){
+
+        if(!useSkill){
+            game.log(player.name + " ไม่ใช้ Assault");
+            return game.resumeDrawPhase();
+        }
+        game.log(player.name + " ใช้ Assault");
+        return true;
     }
 }
