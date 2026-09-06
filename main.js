e1c791a1357ae7ae162dd9b11d203b864bede885
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-06 19:56:39 +0700

    +

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Instance ของเกม โดยกำหนดข้อมูลฮีโร่ ชื่อผู้เล่น และชนิด Controller ของแต่ละคน
 const game = new Game([
     {
-        hero: ZhangLiao, 
+        hero: LiuBei, 
         controller: HumanController//AIController//
     },
     {
-        hero: LiuBei, 
+        hero: ZhangLiao, 
         controller: HumanController//AIController//
     },
     {
