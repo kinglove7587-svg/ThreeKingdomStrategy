3d70ccc2426e420c281f22462b8d8c9983a9e846
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-08 16:24:12 +0700

    +

--- js/Heroes/liuBei.js
+++ js/Heroes/liuBei.js
@@ -22,7 +22,7 @@ class LiuBei extends Player{ // เล่าปี่
         //this.hand.addCard(new BarbarianCard("♠️", 1));
         //this.hand.addCard(new ShadowrunnerCard("♠️", 1));
         //this.hand.addCard(new LightningCard("♠️", 1));
-        //this.hand.addCard(new SlashCard("♠️", 1));
+        //this.hand.addCard(new SlashCard("♥️", 1));
     }
     getPortrait(){
         return "assets/cards/heroes/liuBei.png";
