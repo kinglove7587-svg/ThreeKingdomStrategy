dbd7e03943d526bc279a97f80aabc39a36c7ae3d
kinglove7587-svg <kinglove7587@gmail.com>
2026-09-09 14:09:24 +0700

    +

--- js/debug/debugTools.js
+++ js/debug/debugTools.js
@@ -35,7 +35,7 @@ class DebugTools {
             { hero: HuaXiong, name: "ฮัวหยง"}, 
             { hero: XuZhu, name: "เคาทู"}, 
             { hero: ZhaoYun, name: "จูล่ง"}, 
-            { hero: ZhouYu, name: จิวยี่}
+            { hero: ZhouYu, name: "จิวยี่"}
 
         ];
         // สร้าง Select ให้ Player ทั้ง 5 คน
